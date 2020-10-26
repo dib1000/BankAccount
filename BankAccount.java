03942dfcaa9300e32e136f4295be843ae4243c26
dib1000 <dib5017@gmail.com>
2020-10-26 19:19:27 -0400

    I saw that my constructor method compiled

---
+++ BankAccount.java
@@ -0,0 +1,10 @@
+public class BankAccount {
+  private double balance;
+  private int accountID;
+  private String password;
+  public BankAccount(double b, int a, String p) {
+    balance = b;
+    accountID = a;
+    password = p;
+  }
+}
---
+++ Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+  public static void main(String[] args) {
+    BankAccount b1 = new BankAccount(0.0,1,"help");
+    BankAccount b2 = new BankAccount(542.32,942,"password");
+  }
+}
